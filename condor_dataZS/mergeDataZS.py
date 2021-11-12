037481a47ce2d6e56a3c8f1ab2d92ecadf3979b9
Ravindra Verma <rverma@cmslpc114.fnal.gov>
2021-11-12 14:29:05 -0600

    updated buffer plotting code

--- condor_dataZS/mergeDataZS.py
+++ condor_dataZS/mergeDataZS.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 fPath_1 = '/eos/uscms/store/user/rverma/Output/cms-hgcal-econd/dataZS'
 fPath_2 = 'root://cmseos.fnal.gov//store/user/rverma/Output/cms-hgcal-econd/dataZS'
-branchList = ['entry','layer','waferu','waferv','HDM','TotalWords']
 f = open("gsd_ttbar_path.txt", "r")
 i=0
 for line in f:
@@ -11,9 +10,9 @@ for line in f:
     fileName = f'%s/dataZS_%s'%(fPath_1, name.replace("root", "csv"))
     print(i, fileName)
     if i==0:
-        daq_Data = pd.read_csv(fileName)[branchList]
+        daq_Data = pd.read_csv(fileName)
     else:
-        daq_Data = pd.concat([daq_Data, pd.read_csv(fileName)[branchList]])
+        daq_Data = pd.concat([daq_Data, pd.read_csv(fileName)], ignore_index=True)
     i+=1
 print(len(daq_Data))
 outFile = "dataZS_merged.csv"
